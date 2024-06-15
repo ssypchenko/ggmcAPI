725cb16787f7ffc1273588fcac2cb7209e573c83
ssypchenko <30598453+ssypchenko@users.noreply.github.com>
2024-06-15 12:38:04 +0100

    Initial commit

---
+++ ggmcAPI.cs
@@ -0,0 +1,9 @@
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Capabilities;
+using CounterStrikeSharp.API.Core.Plugin;
+
+public interface MCIAPI
+{
+    public bool GGMC_IsVoteInProgress();
+}
