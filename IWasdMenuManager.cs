5f787f3e0e22b95d41c0b3b97580c321a20ce1e1
ssypchenko <30598453+ssypchenko@users.noreply.github.com>
2024-12-30 12:05:27 +0000

    v1.6.0

---
+++ IWasdMenuManager.cs
@@ -0,0 +1,18 @@
+﻿using CounterStrikeSharp.API.Core;
+namespace MapChooserAPI;
+
+public interface IWasdMenuManager
+{
+    //create new menu object
+    public IWasdMenu CreateMenu(string title = "", bool freezePlayer = true);
+    //open the menu as main menu
+    public void OpenMainMenu(CCSPlayerController? player, IWasdMenu? menu);
+    //close all menus
+    public void CloseMenu(CCSPlayerController? player);
+    //close current submenu and go to previous menu/submenu
+    public void CloseSubMenu(CCSPlayerController? player);
+    //close all submenus and go to main menu
+    public void CloseAllSubMenus(CCSPlayerController? player);
+    //open menu as submenu
+    public void OpenSubMenu(CCSPlayerController? player, IWasdMenu? menu);
+}
--- ggmcAPI.cs
+++ ggmcAPI.cs
@@ -2,8 +2,10 @@ using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Capabilities;
 using CounterStrikeSharp.API.Core.Plugin;
-
+namespace MapChooserAPI;
 public interface MCIAPI
 {
+    event Action CanVoteEvent;
     public bool GGMC_IsVoteInProgress();
+    public void UpdateMapWeights(Dictionary<string, int> newWeights);
 }
